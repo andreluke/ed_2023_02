2a2649d00b74e22922c4a8392e50f47efd225f0e
André Lucas <65183202+andreluke@users.noreply.github.com>
2023-08-25 20:01:41 -0300

    Update ex1.9_alas.ts

--- ex1.9_alas.ts
+++ ex1.9_alas.ts
@@ -1,6 +1,8 @@
 /* P.1.9. Repita o exercício P.1.8 mas utilizando um bloco while, imprimindo os números em
 ordem decrescente. */
 
+/* Nomes: André Lucas de Almeida Sales e Danielle Sismon */
+
 let num:number = 99
 
 while(num>=0){
