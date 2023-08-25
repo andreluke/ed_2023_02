680c972c01e3186382809ec7ef370f329b82711e
André Lucas <65183202+andreluke@users.noreply.github.com>
2023-08-25 20:01:06 -0300

    Create ex1.9_alas.ts
    resolução do ex.1.9 da aula de estrutura de banco de dados feito por André Lucas de Almeida Sales e Danielle Sismon

---
+++ ex1.9_alas.ts
@@ -0,0 +1,9 @@
+/* P.1.9. Repita o exercício P.1.8 mas utilizando um bloco while, imprimindo os números em
+ordem decrescente. */
+
+let num:number = 99
+
+while(num>=0){
+    console.log(num);
+    num -= 3
+}
