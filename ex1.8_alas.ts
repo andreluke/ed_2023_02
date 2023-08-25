176dee77bb5d283be4944406fa832be005221544
André Lucas <65183202+andreluke@users.noreply.github.com>
2023-08-25 20:27:50 -0300

    Create ex1.8_alas.ts
    resposta do exercicio 1.8 feito por André Lucas de Almeida Sales e Danielle Sismon

---
+++ ex1.8_alas.ts
@@ -0,0 +1,8 @@
+/* P.1.8. Escreva um programa, utilizando o bloco for, que imprima todos os múltiplos de 3 contidos entre 0 e 100, inclusive. Os valores devem ser impressos em ordem crescente.  */
+
+/* Nomes: André Lucas de Almeida Sales e Danielle Sismon */
+
+let n:number
+for(n = 0; n<100; n+=3){ 
+    console.log(n)
+}
