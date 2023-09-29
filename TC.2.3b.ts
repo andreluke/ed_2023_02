07807853a1238d10d87df3daa54a45ebff76ec55
André Lucas <65183202+andreluke@users.noreply.github.com>
2023-09-29 20:16:47 -0300

    Update TC.2.3b.ts

--- TC.2.3b.ts
+++ TC.2.3b.ts
@@ -1,17 +1,11 @@
-class FatorialCalculador1 {
-    calcularFatorial1(n: number): number {
-        if (n === 0 || n === 1) {
-            return 1;
-        } else {
-            let resultado = 1;
-            for (let i = 2; i <= n; i++) {
-                resultado *= i;
-            }
-            return resultado;
-        }
-    }
+function FatorialCalcular1(n: number): number {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * FatorialCalcular1(n - 1);
+  }
 }
 
-const fatorialCalculador1 = new FatorialCalculador1();
-const resultado1 = fatorialCalculador1.calcularFatorial1(5);
-console.log("Resultado do fatorial:", resultado1);
+// Exemplo de uso da função:
+const resultado = FatorialCalcular1(5); // Isso calculará o fatorial de 5
+console.log(resultado); // O resultado será 120
